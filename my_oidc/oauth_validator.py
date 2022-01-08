8686c493c1003584634373818b9cd2e806ae7b72
Alan Crosswell <alan@columbia.edu>
2022-01-08 15:53:32 -0500

    Try out DOT PR#1069 version of get_additional_claims with callables.

--- my_oidc/oauth_validator.py
+++ my_oidc/oauth_validator.py
@@ -3,9 +3,9 @@ from oauth2_provider.oauth2_validators import OAuth2Validator
 
 class CustomOAuth2Validator(OAuth2Validator):
 
-    def get_additional_claims(self, request):
+    def get_additional_claims(self):
         return {
-            "sub": request.user.email,
-            "first_name": request.user.first_name,
-            "last_name": request.user.last_name,
+            "sub": lambda request: request.user.email,
+            "first_name": lambda request: request.user.first_name,
+            "last_name": lambda request: request.user.last_name,
         }
