ba0912f6baea02e30d41696ba483123e5d3a9135
Alan Crosswell <alan@columbia.edu>
2022-01-09 12:14:04 -0500

    Comment out experimental feature.

--- my_oidc/oauth_validator.py
+++ my_oidc/oauth_validator.py
@@ -2,10 +2,12 @@ from oauth2_provider.oauth2_validators import OAuth2Validator
 
 
 class CustomOAuth2Validator(OAuth2Validator):
+    pass
 
-    def get_additional_claims(self):
-        return {
-            # "sub": lambda request: request.user.email,
-            "first_name": lambda request: request.user.first_name,
-            "last_name": lambda request: request.user.last_name,
-        }
+    # This is for #1069:
+    # def get_additional_claims(self):
+    #     return {
+    #         # "sub": lambda request: request.user.email,
+    #         "first_name": lambda request: request.user.first_name,
+    #         "last_name": lambda request: request.user.last_name,
+    #     }
